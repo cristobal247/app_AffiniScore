0132fcc6af0f687af23d6fe11d6aabe1df91ccc3
dongyah <bel.toloza@duocuc.cl>
2026-05-19 11:53:52 -0400

    fix: override WebView onPermissionRequest to grant audio capture permission

--- producto/app_AffiniScore/frontend/android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ producto/app_AffiniScore/frontend/android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,5 +1,20 @@
 package io.ionic.starter;
 
+import android.os.Bundle;
+import android.webkit.PermissionRequest;
 import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.BridgeWebChromeClient;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        this.bridge.getWebView().setWebChromeClient(new BridgeWebChromeClient(this.bridge) {
+            @Override
+            public void onPermissionRequest(final PermissionRequest request) {
+                request.grant(request.getResources());
+            }
+        });
+    }
+}
